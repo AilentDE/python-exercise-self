62a009624232a00e9aeb052985ce988d1269aa8f
DE Hsu <dehsu@DEdeMacBook-Pro.local>
2024-12-06 17:23:18 +0800

    Fix table creating issue

--- practice-compare-robyn/gin-api/config/db_method/database.go
+++ practice-compare-robyn/gin-api/config/db_method/database.go
@@ -6,14 +6,27 @@ import (
 )
 
 func CreateTables() {
-	for _, m := range []interface{}{
-		&models.ReadHistory{},
-		&models.Message{},
-		&models.MessagePremission{},
-		&models.UserSubscription{},
-		&models.User{},
-	} {
-		config.DB.Migrator().DropTable(m)
-		config.DB.AutoMigrate(m)
-	}
+	// [WARNNING] Doesn't know the reason that read history table would not be created sometimes
+	// for _, m := range []interface{}{
+	// 	&models.ReadHistory{},
+	// 	&models.Message{},
+	// 	&models.MessagePremission{},
+	// 	&models.UserSubscription{},
+	// 	&models.User{},
+	// } {
+	// 	config.DB.Migrator().DropTable(m)
+	// 	config.DB.AutoMigrate(m)
+	// }
+
+	config.DB.Migrator().DropTable(&models.ReadHistory{})
+	config.DB.Migrator().DropTable(&models.Message{})
+	config.DB.Migrator().DropTable(&models.MessagePremission{})
+	config.DB.Migrator().DropTable(&models.UserSubscription{})
+	config.DB.Migrator().DropTable(&models.User{})
+
+	config.DB.AutoMigrate(&models.ReadHistory{})
+	config.DB.AutoMigrate(&models.Message{})
+	config.DB.AutoMigrate(&models.MessagePremission{})
+	config.DB.AutoMigrate(&models.UserSubscription{})
+	config.DB.AutoMigrate(&models.User{})
 }
